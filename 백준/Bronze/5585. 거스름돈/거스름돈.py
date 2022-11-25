61012927847d1d63a24feb4e8474f2925fc204e8
Ha-Bu <rldud112@naver.com>
2022-11-25 18:24:21 +0900

    [Bronze II] Title: 거스름돈, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/5585. 거스름돈/거스름돈.py
+++ 백준/Bronze/5585. 거스름돈/거스름돈.py
@@ -1,22 +1,8 @@
-n = int(input())
-result = 0
+n = 1000 - int(input())
+count = 0
 
-n = 1000 - n
+for i in [500, 100, 50, 10, 5, 1]:
+    count += n // i
+    n %= i
 
-result += n//500
-n %= 500
-
-result += n//100
-n %= 100
-
-result += n//50
-n %= 50
-
-result += n//10
-n %= 10
-
-result += n//5
-n %= 5
-
-result += n
-print(result)
+print(count)
