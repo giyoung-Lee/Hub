4a6e2accab8a693b5c8fd2642a964b125ec637e2
Ha-Bu <rldud112@naver.com>
2022-11-25 18:21:15 +0900

    [Bronze II] Title: 거스름돈, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/5585. 거스름돈/거스름돈.py
@@ -0,0 +1,22 @@
+n = int(input())
+result = 0
+
+n = 1000 - n
+
+result += n//500
+n %= 500
+
+result += n//100
+n %= 100
+
+result += n//50
+n %= 50
+
+result += n//10
+n %= 10
+
+result += n//5
+n %= 5
+
+result += n
+print(result)
