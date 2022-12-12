f1cb7653de2725f7c29e6df808fa6e3245d5f47f
Ha-Bu <rldud112@naver.com>
2022-12-12 20:54:40 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 4846.06 ms, Memory: 11.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+def solution(s):
+    answer = []
+    _list = deque()
+    flag = True
+
+    for i in range(len(s)):
+        if s[i] not in _list:
+            answer.append(-1)
+            _list.append(s[i])
+        else:
+            for j in reversed(range(len(_list))):
+                if _list[j] == s[i] and flag is True:
+                    answer.append(i - j)
+                    _list.append(s[i])
+                    flag = False
+        flag = True
+
+    return answer
