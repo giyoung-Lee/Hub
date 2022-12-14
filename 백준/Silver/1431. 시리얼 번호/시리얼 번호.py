fc4846bca153cd76eecadf1a2ed7e9c90aa2409c
Ha-Bu <rldud112@naver.com>
2022-12-14 16:36:02 +0900

    [Silver III] Title: 시리얼 번호, Time: 64 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/1431. 시리얼 번호/시리얼 번호.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+
+def digit_sum(str):
+    result = 0
+    for x in str:
+        if x.isdigit():
+            result += int(x)
+
+    return result
+
+
+n = int(input())
+arr = []
+
+for i in range(n):
+    arr.append(input().strip())
+
+arr.sort(key=lambda x: (len(x), digit_sum(x), x))
+
+for i in arr:
+    print(i)
