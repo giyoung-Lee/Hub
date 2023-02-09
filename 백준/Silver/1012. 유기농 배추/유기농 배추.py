87dc7b5db6ad4d08f4d0782afc6806c7133518cb
Ha-Bu <rldud112@naver.com>
2023-02-09 16:19:38 +0900

    [Silver II] Title: 유기농 배추, Time: 228 ms, Memory: 167220 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,35 +1,37 @@
 import sys
-sys.setrecursionlimit(100000)
+sys.setrecursionlimit(10000)
 
+T = int(input())
+
+dx, dy = [1, 0, -1, 0], [0, 1, 0, -1]
 
 def dfs(x, y):
-    visited[x][y] = True
-    direction = [(0, -1), (0, 1), (-1, 0), (1, 0)]
-
-    for dx, dy in direction:
-        nx = dx + x
-        ny = dy + y
-        if nx < 0 or nx >= n or ny < 0 or ny >= m:
+    ch[x][y] = 1
+    for _ in range(4):
+        xx, yy = x + dx[_], y + dy[_]
+        if arr[xx][yy] == 0 or ch[xx][yy]:
             continue
-        if array[nx][ny] and not visited[nx][ny]:
-            dfs(nx, ny)
+        dfs(xx,yy)
 
 
-for _ in range(int(input())):
-    m, n, k = map(int, input().split())
+for i in range(T):
+    arr, ch = [], []
+    M, N, K = map(int, input().split())
+    arr = [[0 for i in range(M+2)] for _ in range(N+2)]
+    ch = [[0 for i in range(M+2)] for _ in range(N+2)]
 
-    visited = [[False] * m for _ in range(n)]
-    array = [[0] * m for _ in range(n)]
-    result = 0
+    for _ in range(K):
+        x, y = map(int, input().split())
+        arr[y+1][x+1] = 1
+    
 
-    for _ in range(k):
-        y, x = map(int, input().split())
-        array[x][y] = 1
+    ans = 0
+    for i in range(1, N+1):
+        for j in range(1, M+1):
+            if arr[i][j] == 0 or ch[i][j]:
+                continue
+            dfs(i, j)
+            ans += 1
+    print(ans)
 
-    for i in range(n):
-        for j in range(m):
-            if array[i][j] and not visited[i][j]:
-                dfs(i, j)
-                result += 1
 
-    print(result)
