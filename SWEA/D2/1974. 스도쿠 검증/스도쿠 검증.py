f59c1d04c8799f3c568735de26ec120ae1a3f40a
Ha-Bu <rldud112@naver.com>
2023-07-15 20:13:58 +0900

    [D2] Title: 스도쿠 검증, Time: 97 ms, Memory: 46,644 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
@@ -1,34 +1,92 @@
-def check_sudoku(puzzle):
-    # 가로, 세로, 작은 격자에 사용된 숫자들을 체크하기 위한 set
-    row_set = [set() for _ in range(9)]
-    col_set = [set() for _ in range(9)]
-    box_set = [set() for _ in range(9)]
+import java.util.Scanner;
+import java.io.FileInputStream;
+import java.util.HashSet;
+import java.util.Set;
 
-    for i in range(9):
-        for j in range(9):
-            num = puzzle[i][j]
-            box_index = (i // 3) * 3 + (j // 3)
-            if num in row_set[i] or num in col_set[j] or num in box_set[box_index]:
-                # 숫자가 이미 사용되었다면 스도쿠가 성립하지 않으므로 0을 리턴
-                return 0
-            else:
-                row_set[i].add(num)
-                col_set[j].add(num)
-                box_set[box_index].add(num)
+class Solution
+{
+        /**
+     * <pre>
+     *     SWEA 스도쿠 검증
+     *     가로, 세로, 격자 한칸이 전부 스도쿠 규칙에 맞는지 확인하는 문제입니다.
+     *     더 좋은 방법이 있겠지만, 9*9로 크기가 고정된 스도쿠이므로,
+     *     존재하는 가로줄(9) + 존재하는 세로줄(9) + 각 격자칸(9)만 비교하면 되겠습니다.
+     *
+     *     Hashset 자료형을 이용하여, 각경우를 비교할때 Hashset에 추가합니다.
+     *     한줄 또는 한칸을 비교했다면, 그 크기가 9인지 비교하면 된다고 생각했습니다.
+     *
+     *     오늘 Hashset을 알아보면서 알게되었는데, 굳이 비교할 필요없이
+     *     값을 add 하려고 할때, 만약 이미 존재하는 값이라면, 자체적으로 false를 리턴하게 됩니다.
+     *     그렇기 때문에 set.add 를 if문 안에 넣고, false일때 false값을 리턴해주면
+     *     굳이 크기가 9인지 비교할 필요가 없어져 절차가 하나 줄어들게됩니다.
+     *
+     *     마지막 정답을 출력하기전에
+     *     경우에 따라 0또는 1을 출력하면 되겠습니다.
+     *
+     * </pre>
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt(); // 테스트 케이스 개수
 
-    # 모든 숫자가 사용되지 않았다면 스도쿠가 성립하지 않으므로 0을 리턴
-    return 1
+        for (int t = 1; t <= T; t++) {
+            int[][] puzzle = new int[9][9]; // 스도쿠 퍼즐 배열 초기화
 
+            // 스도쿠 퍼즐 입력 받기
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    puzzle[i][j] = sc.nextInt();
+                }
+            }
 
-# 입력 받기
-T = int(input())
-puzzles = []
-for _ in range(T):
-    puzzle = [list(map(int, input().split())) for _ in range(9)]
-    puzzles.append(puzzle)
+            int answer = check(puzzle) ? 1 : 0; // 스도쿠 퍼즐 유효성 검사
 
-# 스도쿠 체크하기
-for t in range(T):
-    puzzle = puzzles[t]
-    ans = check_sudoku(puzzle)
-    print(f"#{t+1} {ans}")
+            // 결과 출력
+            System.out.println("#" + t + " " + answer);
+        }
+    }
+
+    // 스도쿠 퍼즐 유효성 검사 함수
+    public static boolean check(int[][] puzzle) {
+
+        // 가로 방향 검사
+        for (int i = 0; i < 9; i++) {
+            Set<Integer> set = new HashSet<>();
+            for (int j = 0; j < 9; j++) {
+                int num = puzzle[i][j];
+                if (!set.add(num)) {
+                    return false;
+                }
+            }
+        }
+
+        // 세로 방향 검사
+        for (int j = 0; j < 9; j++) {
+            Set<Integer> set = new HashSet<>();
+            for (int i = 0; i < 9; i++) {
+                int num = puzzle[i][j];
+                if (!set.add(num)) {
+                    return false;
+                }
+            }
+        }
+
+        // 3x3 작은 격자 검사
+        for (int startRow = 0; startRow < 9; startRow += 3) {
+            for (int startCol = 0; startCol < 9; startCol += 3) {
+                Set<Integer> set = new HashSet<>();
+                for (int i = startRow; i < startRow + 3; i++) {
+                    for (int j = startCol; j < startCol + 3; j++) {
+                        int num = puzzle[i][j];
+                        if (!set.add(num)) {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+
+        // 모든 검사를 통과하면 true
+        return true;
+    }
+}
