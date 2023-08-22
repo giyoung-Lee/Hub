6558e4397ef6335e66623444e8ae19c788d256ab
Ha_Bu <rldud112@naver.com>
2023-08-22 23:27:31 +0900

    [Gold V] Title: ABCDE, Time: 236 ms, Memory: 18952 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.java
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -1,85 +1,88 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-public class Main {
-	
-	/**
-	 * BJ 13023 ABCDE
-	 * 
-	 * dfs로 풀었습니다.
-	 * 인접리스트에 친구들의 정보를 넣고
-	 * 친구 0 번부터 N-1번까지 전부 dfs를 실행하는데,
-	 * 만약 가던 도중 조건에 맞는 경우가 있다면
-	 * 정답을 출력합니다.
-	 * 
-	 * 메모리 : 25,548 kb
-	 * 시간 : 296 ms
-	 */
+/**
 
-    static int N, M;
-    static boolean visited[];
-    static ArrayList<Integer>[] graph;
-    static int answer;
+ <BJ_13023_ABCDE>
+ @author 유세진 * */
+public class Main {
+
+    static Node[] adjList; // 정점(N명의 친구) 개수만큼 인접리스트 생성
+    static boolean[] visited; // 방문체크 배열
+    //static int count = 0; // 방문한 친구 정점 수 카운트
+
+    static class Node {
+        int vertex; // 정점
+        Node next; // 연결리스트에서 다음 노드의 주소 참조
+
+        public Node(int vertex, Node next) {
+            this.vertex = vertex;
+            this.next = next;
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        // 입력받기
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        visited = new boolean[N];
-        graph = new ArrayList[N];
+        int N = Integer.parseInt(st.nextToken()); // 사람의 수 (5~2000)
+        int M = Integer.parseInt(st.nextToken()); // 친구 관계의 수 (1~2000)
 
-        // 그래프 초기화
-        for (int i = 0; i < N; i++) {
-            graph[i] = new ArrayList<>();
-        }
+        adjList = new Node[N]; // 정점(N명의 친구) 개수만큼 인접리스트 생성
+        visited = new boolean[N]; // 방문체크 배열 초기화
 
-        for (int i = 0; i < M; i++) {
-        	// 양방향 그래프이므로, 양쪽에 다 넣어줌
+        // 친구 관계 입력받아 인접리스트 생성
+        for(int i=0; i<M; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
+            int from = Integer.parseInt(st.nextToken()); // 시작 정점
+            int to = Integer.parseInt(st.nextToken()); // 도착 정점
 
-            graph[b].add(a);
-            graph[a].add(b);
+            adjList[from] = new Node(to, adjList[from]); // 0 1 입력 받을 때 0번 노드에 1번 노드를 연결
+            adjList[to] = new Node(from, adjList[to]); // 1번 노드에도 0번 노드를 연결해줘야 함
         }
 
-        for (int i = 0; i < N; i++) {
-        	// 탐색결과가 true면 그만 탐색하고 정답 출력
-            if(dfs(i, 1)) {
-            	System.out.println(1);
-            	return;
+        for(int i=0; i<N; i++) {
+            //flag = dfs(adjList[i], 0); // 인접리스트의 첫번째 노드와 시작정점 전달
+            // 인접리스트의 첫번째 노드와 시작정점 전달
+            if (dfs(i, 1)) {
+                System.out.println("1");
+                return;
             }
             visited = new boolean[N];
         }
-        // 만약 dfs를 다 돌았는데 true를 받지못했다면 0
-        System.out.println(0);
+
+        System.out.println("0");
     }
 
-    private static boolean dfs(int i, int count) {
-    	// 방문처리
-        visited[i] = true;
-        // 조건에 맞게되면 true 리턴
-        if (count >= 5) {
-        	return true;
+    // current : 현재 정점
+    // count : 방문한 친구 정점의 개수
+    private static boolean dfs(int current, int count) {
+
+        visited[current] = true; // 현재 정점 방문 처리
+        //count++; // 방문한 친구 정점 수 증가
+
+        // 방문한 친구 정점이 다섯개이면
+        if(count >= 5) {
+            // 더 이상 탐색하지 않아도 이미 조건과 같은 친구 관계가 성립되고 true 반환
+            return true;
         }
-        
-        for (int neighbor : graph[i]) {
-            if (!visited[neighbor]) {
-            	// 들어갔던 dfs가 true면 다시한번 true리턴
-                if(dfs(neighbor, count + 1)) {
-                	return true;
+
+        // 현재 정점에 인접한 정점들 탐색
+        for(Node temp=adjList[current]; temp!=null; temp=temp.next) {
+            // 인접한 정점들 중 아직 방문하지 않았으면
+            if(!visited[temp.vertex]) {
+                // 정점 방문
+                if (dfs(temp.vertex, count+1)) {
+                    return true;
                 }
-                // 방문했다가 나온곳은 다시 false로 바꿔줌
-                visited[neighbor] = false;
+                visited[temp.vertex] = false;
             }
         }
-        // 다 왔다갔다 했지만, 단한번도 true를 못만났다면 false;
-        return false;
+
+        return false; // 모든 친구 정점들을 다 탐색했는데 조건을 만족하지 못하면 false 반환
+
     }
+
 }
