73e48b692a14f6f77c5e0506c3b3da7627d9fb77
Ha_Bu <rldud112@naver.com>
2023-08-28 11:26:58 +0900

    [Unrated] Title: [SW Test 샘플문제] 프로세서 연결하기, Time: 151 ms, Memory: 27,132 KB -BaekjoonHub

--- SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
+++ SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
@@ -1,149 +1,115 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
+
 public class Solution {
-    /**
-     * 
-     * 
-     */
 
-    static int N, size, min;
-    static int[][] arr;
-    static core[] Core;
-    static boolean used_core[], visited[][];
-    static int[] dx = { 1, -1, 0, 0 };
-    static int[] dy = { 0, 0, 1, -1 };
+	static int N, max,totalCnt, min,map[][];
+	static int[] dr = {-1,1,0,0};
+	static int[] dc = {0,0,-1,1};
+	static ArrayList<int[]> list; //  전선을 놓아야할 코어리스트
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int TC = Integer.parseInt(in.readLine());
+		for (int tc = 1; tc <= TC; tc++) {
+			N = Integer.parseInt(in.readLine()); // 멕시노스 크기
+			map = new int[N][N];
+			max = 0;
+			min = Integer.MAX_VALUE;
+			totalCnt = 0; // 연결해야하는 코어 개수
+			list = new ArrayList<int[]>(); // 연결해야하는 코어 리스트
+			
+			for (int i = 0; i < N; i++) {
+				StringTokenizer st = new StringTokenizer(in.readLine()," ");
+				for (int j = 0; j < N; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+					// 가장자리 코어 제외(행이 0, N-1, 열이 0, N-1)
+					if((i==0 || i==N-1 || j==0 || j==N-1) && map[i][j] == 1) continue;
+					if(map[i][j] == 1) { // 코어
+						list.add(new int[] {i,j});
+						totalCnt++;
+					}
+				}
+			}
+			go(0, 0);
+			System.out.println("#"+tc+" "+min);
+		}
+	}
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+	// 코어를 선택(4방향 시도)/비선택
+	private static void go(int index, int coreCnt) { // index : 고려해야할 코어의 index, coreCnt : 연결된 코어 개수
+		// 가지치기 : 현재까지 연결된 코어수 + 남은 코어수 < 임시 최대 코어 연결수 
+		if(coreCnt+(totalCnt-index)<max) return;
+		// 기저조건 처리
+		if(index == totalCnt) {
+			int res = getLength(); // 놓아진 전선의 길이의 합
+			if(max<coreCnt) {
+				max = coreCnt;
+				min = res;
+			}else if(max == coreCnt) {
+				if(min>res) {
+					min = res;
+				}
+			}
+			return;
+		}
+		
+		int[] cur = list.get(index);
+		int r = cur[0];
+		int c = cur[1];
+		
+		// 현재 코어 선택 (4방향 시도)
+		for (int d = 0; d < 4; d++) {
+			// 현재 코어의 위치에서 해당 방향으로 전선 놓기가 가능한지 체크
+			if(!isAvailable(r,c,d)) continue;
+			// 가능하다면 전선 놓기
+			setStatus(r,c,d,2);
+			// 다음 코어로 가기 
+			go(index+1, coreCnt+1);
+			// 새로운 방향을 시도하기 위해 놓았던 전선 지우기
+			setStatus(r,c,d,0);
+		}
+		// 현재 코어 비선택
+		go(index+1, coreCnt);
+	}
+	private static int getLength() {
+		int lCnt = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if(map[i][j]==2) {
+					lCnt++;
+				}
+			}
+		}
+		return lCnt;
+	}
+	private static void setStatus(int r, int c, int d, int status) {
+		int nr = r;
+		int nc = c;
+		
+		while(true) {
+			nr += dr[d];
+			nc += dc[d];
+			if(nr<0 || nr>=N || nc<0 || nc>=N) break;
+			map[nr][nc] = status;
+		}
+	}
 
-        int T = Integer.parseInt(br.readLine());
-        for (int test = 1; test <= T; test++) {
-            // 초기화
-            N = Integer.parseInt(br.readLine());
-            arr = new int[N][N];
-            // 최대 12개이므로
-            Core = new core[12];
-            used_core = new boolean[12];
-            visited = new boolean[N][N];
-            // 코어의 개수
-            size = 0;
-            // 정답
-            min = Integer.MAX_VALUE;
-
-            // 값과 코어 등록
-            for (int i = 0; i < N; i++) {
-                st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < N; j++) {
-                    arr[i][j] = Integer.parseInt(st.nextToken());
-
-                    if (i > 0 && i < N-1 && j > 0 && j < N-1) {
-                        if(arr[i][j] == 1){
-                            Core[size++] = new core(i, j);
-                        }
-                    }
-                }
-            }
-
-            for(int i = size; i >= 0; i--) {
-                comb(0, 0, i);
-                // 한번이라도 갱신 되었다면
-                if(min < Integer.MAX_VALUE){
-                    break;
-                }
-            }
-            System.out.println("#"+ test + " " + min);
-        }
-
-    }
-
-    public static void dfs(int index, int count) {
-        if (index == size) {
-            min = Math.min(min, count);
-            return;
-        }
-    
-        // comb에서 걸러낸 애들만 dfs 실행
-        if (!used_core[index]) {
-            dfs(index+1, count);
-            return;
-        }
-
-        // 4방향 dfs 탐색할건데
-        for (int i = 0; i < 4; i++) {
-            int nx = Core[index].x;
-            int ny = Core[index].y;
-            // 현재위치 방문처리
-            visited[nx][ny] = true;
-            int temp = 0;
-
-            // 탐색을 끝까지 했는지 체크하는 flag
-            boolean flag = false;
-            while (true) {
-                nx = nx + dx[i];
-                ny = ny + dy[i];
-
-                // 끝까지 간 경우
-                if ( nx < 0 || nx >= N || ny < 0 || ny >= N) {
-                    flag = true;
-                    break;
-                }
-
-                // 중간에 전선이나 코어를 만난경우
-                if(arr[nx][ny] == 1 || visited[nx][ny]) {
-                    break;
-                }
-
-                // 지나온 길을 방문처리
-                visited[nx][ny] = true;
-
-                // 전선의 수 ++
-                temp++;
-            }
-
-            // 성공했으면 전선의 수를 넘기며 다음 dfs 실행
-            if(flag) {
-                dfs(index + 1, count + temp);
-            }
-
-            // 다 돌고 왔으면 방문처리 원상태로 돌려놓기
-            while (true) {
-                // 한칸씩돌아가면서
-                nx -= dx[i];
-                ny -= dy[i];
-                if(nx == Core[index].x && ny == Core[index].y) {
-                    break;
-                }
-                // false처리
-                visited[nx][ny] = false;
-            }
-        }
-    }
-
-    // 조합. r 은 nCr 의 r
-    public static void comb(int index, int count, int r) {
-        // 기저조건. 개수가 다 채워지면
-        // dfs 실행
-        if(count == r) {
-            dfs(0, 0);
-            return;
-        }
-        for (int i = index; i < size; i++) {
-            used_core[i] = true;
-            comb(i+1, count+1, r);
-            used_core[i] = false;
-        }
-    }
-
-    static class core {
-        int x, y;
-
-        public core(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
+	private static boolean isAvailable(int r, int c, int d) {
+		int nr = r;
+		int nc = c;
+		
+		while(true) {
+			nr += dr[d];
+			nc += dc[d];
+			if(nr<0 || nr>=N || nc<0 || nc>=N) break;
+			if(map[nr][nc] != 0) return false;
+		}
+		return true;
+	}
 }
