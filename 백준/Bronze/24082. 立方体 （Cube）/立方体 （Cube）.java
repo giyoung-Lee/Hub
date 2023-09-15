522e81139b1ca6dea3912b7930a861821154a01b
Ha_Bu <rldud112@naver.com>
2023-09-16 02:32:58 +0900

    [Bronze V] Title: 立方体 (Cube), Time: 208 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/24082. 立方体 （Cube）/立方体 （Cube）.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        int X = scan.nextInt();
+        System.out.println(X*X*X);
+    }
+}
