bfcf1e90187ea8e104786f407fd3bdbc4e0f5655
Ha_Bu <rldud112@naver.com>
2023-10-04 15:54:39 +0900

    [D4] Title: [Professional] 키 순서, Time: 1,495 ms, Memory: 93,284 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -4,60 +4,65 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		int T = Integer.parseInt(br.readLine());
-		int max = Integer.MAX_VALUE/4;
-		for(int test=1; test<=T; test++) {
-			
-			int N = Integer.parseInt(br.readLine());
-			int M = Integer.parseInt(br.readLine());
-			int[][] arr = new int[N][N];
-			
-			for(int i = 0; i < N; i++) {
-				for (int j = 0; j < N; j++) {
-					if (i==j) {
-						arr[i][j] = 0;
-					} else {
-						// 적당히 큰값
-						arr[i][j] = max;
-					}
-				}
-			}
-			
-			for(int i = 0; i < M; i++) {
-				st = new StringTokenizer(br.readLine());
-				arr[Integer.parseInt(st.nextToken())-1][Integer.parseInt(st.nextToken())-1] = 1;
-			}
-			
-    		for(int i = 0; i < N; i++) {
-    			for(int j = 0; j < N; j++) {
-    				if (i==j) continue;
-    				for(int k = 0; k < N; k++) {
-    					if (j == k || i == k) continue;
-    					
-    					arr[j][k] = Math.min(arr[j][i] + arr[i][k], arr[j][k]);
-    				}
-    			}
-    		}
-    		
-			int[] result = new int[N];
-			
-    		for(int i = 0; i < N; i++) {
-    			for (int j = 0; j < N; j++) {
-    				if(arr[i][j] != max || arr[j][i] != max) {
-    					result[i] ++;
-    				}
-    			}
-    		}
-    		int count = 0;
-    		for(int i = 0; i < N; i++) {
-    			if (result[i] == N) count++;
-    		}
-    		
-    		System.out.println("#"+test + " "+count);
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int test = 1; test <= T; test++) {
+            int N = Integer.parseInt(br.readLine());
+            int M = Integer.parseInt(br.readLine());
+
+            int[][] arr = new int[N][N];
+
+            // Initialize the adjacency matrix with a large value
+            int max = Integer.MAX_VALUE / 4;
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (i == j) {
+                        arr[i][j] = 0;
+                    } else {
+                        arr[i][j] = max;
+                    }
+                }
+            }
+
+            for (int i = 0; i < M; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                int from = Integer.parseInt(st.nextToken()) - 1;
+                int to = Integer.parseInt(st.nextToken()) - 1;
+                arr[from][to] = 1;
+            }
+
+            // Floyd-Warshall algorithm optimization
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (i == j || arr[i][j] == 1) continue;
+                    for (int k = 0; k < N; k++) {
+                        if (j == k || i == k) continue;
+
+                        if (arr[j][k] > arr[j][i] + arr[i][k]) {
+                            arr[j][k] = arr[j][i] + arr[i][k];
+                        }
+                    }
+                }
+            }
+
+            int count = 0;
+            for (int i = 0; i < N; i++) {
+                boolean isConnected = true;
+                for (int j = 0; j < N; j++) {
+                    if (i != j && (arr[i][j] == max && arr[j][i] == max)) {
+                        isConnected = false;
+                        break;
+                    }
+                }
+                if (isConnected) {
+                    count++;
+                }
+            }
+
+            System.out.println("#" + test + " " + count);
+        }
+    }
 }
