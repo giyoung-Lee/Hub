8f9f909ac5e56f426a022c7734b26561cb6940b9
Ha_Bu <rldud112@naver.com>
2023-10-11 14:16:44 +0900

    [D3] Title: [Professional] 조합, Time: 139 ms, Memory: 27,356 KB -BaekjoonHub

--- SWEA/D3/5607. ［Professional］ 조합/［Professional］ 조합.java
+++ SWEA/D3/5607. ［Professional］ 조합/［Professional］ 조합.java
@@ -1,11 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
 import java.util.StringTokenizer;
 
 public class Solution {
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         
@@ -13,10 +12,10 @@ public class Solution {
         int p = 1234567891; // 모듈로 연산에 사용할 소수 p
 
         // 미리 계산된 팩토리얼 값 저장
-        BigInteger[] factorial = new BigInteger[1000001];
-        factorial[0] = BigInteger.ONE;
+        long[] factorial = new long[1000001];
+        factorial[0] = 1;
         for (int i = 1; i <= 1000000; i++) {
-            factorial[i] = factorial[i - 1].multiply(BigInteger.valueOf(i)).mod(BigInteger.valueOf(p));
+            factorial[i] = (factorial[i - 1] * i) % p;
         }
 
         for (int test = 1; test <= T; test++) {
@@ -25,16 +24,27 @@ public class Solution {
             int R = Integer.parseInt(st.nextToken());
 
             // 페르마의 소정리를 적용하여 역수 계산
-            BigInteger rInverse = factorial[R].modPow(BigInteger.valueOf(p - 2), BigInteger.valueOf(p));
-            BigInteger nMinusRInverse = factorial[N - R].modPow(BigInteger.valueOf(p - 2), BigInteger.valueOf(p));
+            long rInverse = modPow(factorial[R], p - 2, p);
+            long nMinusRInverse = modPow(factorial[N - R], p - 2, p);
 
             // 조합 계산
-            BigInteger result = factorial[N];
-            result = result.multiply(rInverse);
-            result = result.multiply(nMinusRInverse);
-            result = result.mod(BigInteger.valueOf(p));
+            long result = (factorial[N] * rInverse) % p;
+            result = (result * nMinusRInverse) % p;
 
             System.out.println("#" + test + " " + result);
         }
-	}
+    }
+
+    // a^b % p 계산
+    private static long modPow(long a, int b, int p) {
+        long result = 1;
+        while (b > 0) {
+            if (b % 2 == 1) {
+                result = (result * a) % p;
+            }
+            a = (a * a) % p;
+            b /= 2;
+        }
+        return result;
+    }
 }
