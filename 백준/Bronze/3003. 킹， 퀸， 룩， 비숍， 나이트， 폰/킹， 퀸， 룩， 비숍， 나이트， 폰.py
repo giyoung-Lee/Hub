6ad8b885a51c2c827c12327b0facc3445408b43b
Ha_Bu <rldud112@naver.com>
2024-07-14 17:44:57 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -1,19 +1,8 @@
+import sys
+input = sys.stdin.readline
+
 arr = list(map(int, input().split()))
-result = []
+temp = [1, 1, 2, 2, 2, 8]
 
-for i in ["k", "q", "l", "b", "n", "p"]:
-    if i == "k":
-        result.append(1 - arr[0])
-    elif i == "q":
-        result.append(1 - arr[1])
-    elif i == "l":
-        result.append(2 - arr[2])
-    elif i == "b":
-        result.append(2 - arr[3])
-    elif i == "n":
-        result.append(2 - arr[4])
-    elif i == "p":
-        result.append(8 - arr[5])
-
-for i in result:
-    print(i, end=' ')
+for i in range(len(arr)):
+    print(temp[i]-arr[i], end=" ")
